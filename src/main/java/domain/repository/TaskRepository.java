4716b72f3fd51285cc1962d4acf9aeb94edebdd1
Priscila Nascimento <pnmelo2808@gmail.com>
2025-10-22 12:25:37 -0300

    Feat: Add TaskRepository interface for task management

---
+++ src/main/java/domain/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package domain.repository;
+
+import domain.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
